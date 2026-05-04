d684d0831d00969c4fba08e8a0813219e778b423
AD <amoldinkar9@Amols-MacBook-Air.local>
2026-05-04 10:38:40 +0530

    Google Tag Manager Integrated

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
   return (
     <html lang="mr" className={cn("antialiased", anekDevanagari.variable, "font-sans", geist.variable)}>
       <head>
-
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-DDQPJQ7VLG" />
         <script
@@ -46,6 +45,18 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PFFP2NSZ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
 
         <script
           type="application/ld+json"
@@ -78,7 +89,13 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className="min-h-screen font-sans">{children}</body>
+      <body className="min-h-screen font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFFP2NSZ"
+          height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   );
 }
